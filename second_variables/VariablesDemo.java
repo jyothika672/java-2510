aa6f5e50d716efefe0b370cb164b6742f94fb566
jyothika672 <katrujyo@gmail.com>
2025-07-23 23:31:27 +0530

    VariableDemo

---
+++ second_variables/VariablesDemo.java
@@ -0,0 +1,28 @@
+package second_variables;
+
+public class VariablesDemo {
+            //brand
+            //descripation
+            //rating
+            //price
+        // if we define variable in class we call them instance variables
+    static int someData1;
+    static double someData2;  //declartion
+    static char someData3; 
+    static boolean someData4;   
+    short price = 350;
+    String brand = "HIGHLANDER";
+    float rating=4.2f;
+    public static void main(String[] args) {
+        //if we define variable in methods we call them as local variables
+        int someDataNew1 = 10; //intilization
+        int someDataNew2; // declaration
+        System.out.println(someDataNew1); 
+        //System.out.println(someDataNew2);
+
+        System.out.println(someData1);
+        System.out.println(someData2);
+        System.out.println(someData3);
+        System.out.println(someData4);
+    }
+}
