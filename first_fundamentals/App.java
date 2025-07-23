7f2319c70a88f7f4ce4624ae8710e29b35cd6a49
jyothika672 <katrujyo@gmail.com>
2025-07-23 13:40:09 +0530

    First program

--- first_fundamentals/App.java
+++ first_fundamentals/App.java
@@ -1,5 +1,11 @@
-package first_fundamentals;
-
-public class App {
-    
+class App{
+    // if you want to run a java program, class and main method are must
+    // public -access modifier
+    // static - no oblect is required to run program
+    // void - return nothing
+    // main - method name, String[] args- its a arrgument for main method
+    public static void main(String[] args) {
+        // method body
+        System.out.println("Hello World");
+    }
 }
