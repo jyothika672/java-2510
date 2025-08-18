bb71941d706ebf09b267ae69b572528cde2eaf85
jyothika672 <katrujyo@gmail.com>
2025-08-18 12:18:32 +0530

    Access Modifiers

---
+++ pack2/OtherPackageTest.java
@@ -0,0 +1,16 @@
+package pack2;
+
+import pack1.Person;
+
+public class OtherPackageTest {
+    public static void main(String[] args) {
+        Person p = new Person();
+        System.out.println("Different package - non subclass");
+        System.out.println(p.publicName); 
+        // We canont access anything expect public if it's on a
+        //Different
+        //System.out.println(p.protectedName); 
+        //System.out.println(p.defaultName);
+        //System.out.println(p.privateName); 
+    }
+}
