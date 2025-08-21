63d979d8afd8dae777f15beaba23b861827ebdbd
jyothika672 <katrujyo@gmail.com>
2025-08-21 18:44:15 +0530

    Abstarct classes

---
+++ thirteen_abstration/User.java
@@ -0,0 +1,33 @@
+package thirteen_abstration;
+
+public class User {
+
+    public static void main(String[] args) {
+        
+        Lenovo lenovo = new Lenovo();
+        System.out.println("Buying Lenovo Laptop");
+        lenovo.usbSlot();
+        lenovo.hdmiSlot();
+        lenovo.cPort();
+        lenovo.microsdSlot();
+
+        System.out.println("Buying Dell Laptop");
+        Dell dell = new Dell();
+        dell.usbSlot();
+        dell.hdmiSlot();
+        dell.cPort();
+        dell.audioJack();
+        dell.microsdSlot();
+        dell.ethernetSlot();
+        Laptop.ethernetSlot();
+        
+        // --> abstract classes cannot be instantiated
+        // NewLenovo newLenovo = new NewLenovo(); 
+      
+    }
+
+    
+    
+
+
+}
---
+++ thirteen_abstration/UserAbs.java
@@ -0,0 +1,28 @@
+package thirteen_abstration;
+
+public class UserAbs {
+
+    public static void main(String[] args) {
+        
+        // Lenovo lenovo = new Lenovo(); --> No Abstraction
+
+        // With Abstraction
+        Laptop lenovo = new Lenovo();
+        System.out.println("Buying Lenovo Laptop");
+        lenovo.usbSlot();
+        lenovo.hdmiSlot();
+        lenovo.cPort();
+        lenovo.microsdSlot();
+
+        System.out.println("Buying Dell Laptop");
+        Dell dell = new Dell();
+        dell.usbSlot();
+        dell.hdmiSlot();
+        dell.cPort();
+        dell.audioJack();
+        dell.microsdSlot();
+        Laptop.ethernetSlot();
+      
+    }
+
+}
